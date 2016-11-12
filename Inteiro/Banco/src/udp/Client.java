f9daab6622bdc53b30ec097a55ebce0ca2363364
psiuzin <mateus_lippstein@hotmail.com>
2016-11-12 14:56:55 -0200

    UDP

--- Inteiro/Banco/src/udp/Client.java
+++ Inteiro/Banco/src/udp/Client.java
@@ -5,30 +5,300 @@
  */
 package udp;
 
+import construtor.BelasMensagens;
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 /**
  *
  * @author psiuzin
  */
 public class Client {
-    public static void main(String args[]) throws Exception
-   {
-      BufferedReader inFromUser =
-         new BufferedReader(new InputStreamReader(System.in));
-      DatagramSocket clientSocket = new DatagramSocket();
-      InetAddress IPAddress = InetAddress.getByName("localhost");
-      byte[] sendData = new byte[1024];
-      byte[] receiveData = new byte[1024];
-      String sentence = inFromUser.readLine();
-      sendData = sentence.getBytes();
-      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 2006);
-      clientSocket.send(sendPacket);
-      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-      clientSocket.receive(receivePacket);
-      String modifiedSentence = new String(receivePacket.getData());
-      System.out.println("Server:" + modifiedSentence);
-      clientSocket.close();
-   }
+
+    private static String[] splitByNumber(String s, int size) { // Separa uma string em várias string de tamanho X
+        if (s == null || size <= 0) {
+            return null;
+        }
+        int chunks = s.length() / size + ((s.length() % size > 0) ? 1 : 0);
+        String[] arr = new String[chunks];
+        for (int i = 0, j = 0, l = s.length(); i < l; i += size, j++) {
+            arr[j] = s.substring(i, Math.min(l, i + size));
+        }
+        return arr;
+    }
+
+    public static void main(String args[]) throws Exception {
+
+        System.out.println("Selecione o tipo de operação que deseja realizar:");
+        System.out.println("1- Adicionar mensagem\n"
+                + "2- Alterar mensagem\n"
+                + "3- Excluir mensagem\n"
+                + "4- Consultar mensagem\n"
+                + "5- Listar mensagem por tipo\n"
+                + "6- Mensagem aleatória conforme tipo\n"
+                + "0- sair");
+        Scanner input = new Scanner(System.in);
+        int opc = input.nextInt();
+        String msg = new String();
+        String msgcod = new String();
+        String tipomsg = new String();
+        String[] sendmsg;
+
+        DatagramSocket clientSocket = new DatagramSocket();
+        InetAddress IPAddress = InetAddress.getByName("localhost");
+        byte[] sendData = new byte[1024];
+        byte[] receiveData = new byte[1024];
+
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 2006);
+        String n = new String();
+        String enviar = new String();
+
+        DatagramSocket serverSocket = new DatagramSocket(2006);
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        
+        String sentence = new String();
+        String[] split;
+        int qtdmsg;
+        
+        switch (opc) {
+            case 1: // Adiciona msg
+                System.out.println("Digite a mensagem:");
+                input = new Scanner(System.in);
+                msg = input.nextLine();
+                sendmsg = splitByNumber(msg, 110); // Splito a mensagem pra saber se precisa de mais de 1 string
+                System.out.println("Digite o código da mensagem:");
+                input = new Scanner(System.in);
+                msgcod = input.nextLine();
+                System.out.println("Digite o tipo da mensagem:");
+                input = new Scanner(System.in);
+                tipomsg = input.nextLine();
+                //System.out.println(sendmsg.length);
+
+                n = new String();
+                n = Integer.toString(sendmsg.length) + "#";
+                sendData = n.getBytes();
+                sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 2006);
+                clientSocket.send(sendPacket); // Enviei quantas mensagens vou precisar
+
+                enviar = new String();
+                enviar += "1#" + msgcod + "#" + tipomsg; // Tipo de operação 1
+                sendData = enviar.getBytes();
+                sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 2006);
+                clientSocket.send(sendPacket); // Enviando o conjunto de dados
+
+                for (int i = 0; i < sendmsg.length; i++) {
+                    sendData = sendmsg[i].getBytes();
+                    sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 2006);
+                    clientSocket.send(sendPacket);
+                }
+
+                break;
+            case 2: // Altera a msg
+                System.out.println("Digite a mensagem:");
+                input = new Scanner(System.in);
+                msg = input.nextLine();
+                sendmsg = splitByNumber(msg, 110);
+                System.out.println("Digite o código da mensagem:");
+                input = new Scanner(System.in);
+                msgcod = input.nextLine();
+                System.out.println("Digite o tipo da mensagem:");
+                input = new Scanner(System.in);
+                tipomsg = input.nextLine();
+                System.out.println(sendmsg.length);
+
+                n = new String();
+                n = Integer.toString(sendmsg.length) + "#";
+                sendData = n.getBytes();
+                sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 2006);
+                clientSocket.send(sendPacket); // Enviei quantas mensagens vou precisar
+
+                enviar = new String();
+                enviar += "2#" + msgcod + "#" + tipomsg; // Tipo de operação 2
+                sendData = enviar.getBytes();
+                sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 2006);
+                clientSocket.send(sendPacket); // Enviando o conjunto de dados
+
+                for (int i = 0; i < sendmsg.length; i++) {
+                    sendData = sendmsg[i].getBytes();
+                    sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 2006);
+                    clientSocket.send(sendPacket);
+                }
+
+                break;
+            case 3: // Exclui 
+                System.out.println("Digite código da msg que deseja excluir:");
+                input = new Scanner(System.in);
+                msgcod = input.nextLine();
+
+                n = new String();
+                n = "1#";
+                sendData = n.getBytes();
+                sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 2006);
+                clientSocket.send(sendPacket); // Enviei quantas mensagens vou precisar
+
+                enviar = new String();
+                enviar += "3#" + msgcod + "#1"; // Tipo de operação 3
+                sendData = enviar.getBytes();
+                sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 2006);
+                clientSocket.send(sendPacket); // Enviando o conjunto de dados
+
+                enviar = "Não tem msg";
+                sendData = enviar.getBytes();
+                sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 2006);
+                clientSocket.send(sendPacket); // Envia mensagem em branco para acoplar ao padrão de comunicação
+
+                break;
+            case 4: // Consultar mensagem
+                System.out.println("Digite o código da mensagem que deseja consultar:");
+                input = new Scanner(System.in);
+                msgcod = input.nextLine();
+
+                n = new String();
+                n = "1#";
+                sendData = n.getBytes();
+                sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 2006);
+                clientSocket.send(sendPacket); // Enviei quantas mensagens vou precisar
+
+                enviar = new String();
+                enviar += "4#" + msgcod + "#1"; // Tipo de operação 4
+                sendData = enviar.getBytes();
+                sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 2006);
+                clientSocket.send(sendPacket); // Enviando o conjunto de dados
+
+                enviar = "Não tem msg";
+                sendData = enviar.getBytes();
+                sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 2006);
+                clientSocket.send(sendPacket);
+
+                // Começo a receber os pacotes da mensagem consultada
+                receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                serverSocket.receive(receivePacket);
+                sentence = new String(receivePacket.getData()); // Recebi o tamanho da mensagem
+                msg = new String();
+                //BelasMensagens bmsg = new BelasMensagens();
+
+                split = sentence.split("#");
+                qtdmsg = Integer.parseInt(split[0]); // Recebi a quantidade de pacotes necessários p/ a msg
+
+                for (int i = 0; i < qtdmsg; i++) {
+                    serverSocket.receive(receivePacket);
+                    sentence = new String(receivePacket.getData());
+                    msg = new String();
+                    msg += sentence;
+                }
+                System.out.println("Sua mensagem é: "+msg);
+
+                break;
+            case 5: // Lista por tipo
+                System.out.println("Digite o tipo de mensagem que deseja consultar:");
+                input = new Scanner(System.in);
+                tipomsg = input.nextLine();
+
+                n = new String();
+                n = "1#";
+                sendData = n.getBytes();
+                sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 2006);
+                clientSocket.send(sendPacket); // Enviei quantas mensagens vou precisar
+
+                enviar = new String();
+                enviar += "5#1#" + tipomsg; // Tipo de operação 5
+                sendData = enviar.getBytes();
+                sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 2006);
+                clientSocket.send(sendPacket); // Enviando o conjunto de dados
+
+                enviar = "Não tem msg";
+                sendData = enviar.getBytes();
+                sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 2006);
+                clientSocket.send(sendPacket);
+                
+                // Começo a receber os pacotes da lista consultada
+                receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                serverSocket.receive(receivePacket);
+                sentence = new String(receivePacket.getData()); // Recebi o tamanho da lista
+                split = sentence.split("#");
+                int tamlista = Integer.parseInt(split[0]);
+                
+                for (int i=0; i<tamlista; i++)
+                {
+                    receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                serverSocket.receive(receivePacket);
+                sentence = new String(receivePacket.getData()); // Recebi o tamanho da mensagem
+                msg = new String();
+                //BelasMensagens bmsg = new BelasMensagens();
+
+                split = sentence.split("#");
+                qtdmsg = Integer.parseInt(split[0]); // Recebi a quantidade de pacotes necessários p/ a msg
+
+                for (int j = 0; j < qtdmsg; j++) {
+                    serverSocket.receive(receivePacket);
+                    sentence = new String(receivePacket.getData());
+                    msg = new String();
+                    msg += sentence;
+                }
+                    System.out.println("Mensagem "+i+": "+msg);
+                }
+
+                break;
+            case 6: // Msg Aleatória por tipo
+                System.out.println("Digite o tipo de mensagem que deseja:");
+                input = new Scanner(System.in);
+                tipomsg = input.nextLine();
+
+                n = "1#";
+                sendData = n.getBytes();
+                sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 2006);
+                clientSocket.send(sendPacket); // Enviei quantas mensagens vou precisar
+
+                enviar = new String();
+                enviar += "6#1#" + tipomsg; // Tipo de operação 6
+                sendData = enviar.getBytes();
+                sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 2006);
+                clientSocket.send(sendPacket); // Enviando o conjunto de dados
+
+                enviar = "Não tem msg";
+                sendData = enviar.getBytes();
+                sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 2006);
+                clientSocket.send(sendPacket);
+
+                // Começo a receber os pacotes da mensagem consultada
+                receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                serverSocket.receive(receivePacket);
+                sentence = new String(receivePacket.getData()); // Recebi o tamanho da mensagem
+                msg = new String();
+                //BelasMensagens bmsg = new BelasMensagens();
+
+                split = sentence.split("#");
+                qtdmsg = Integer.parseInt(split[0]); // Recebi a quantidade de pacotes necessários p/ a msg
+
+                for (int i = 0; i < qtdmsg; i++) {
+                    serverSocket.receive(receivePacket);
+                    sentence = new String(receivePacket.getData());
+                    msg = new String();
+                    msg += sentence;
+                }
+                System.out.println("Sua mensagem é: "+msg);
+                
+                break;
+            case 7:
+                break;
+
+            default:
+                System.out.println("Opção inválida");
+        }
+        
+//        System.out.println("Digite a mensagem");
+//        BufferedReader inFromUser
+//                = new BufferedReader(new InputStreamReader(System.in));
+
+//        String sentence = inFromUser.readLine();
+//        sendData = sentence.getBytes();
+//        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 2006);
+//        clientSocket.send(sendPacket);
+//        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+//        clientSocket.receive(receivePacket);
+//        String modifiedSentence = new String(receivePacket.getData());
+//        System.out.println("Server:" + modifiedSentence);
+//        clientSocket.close();
+    }
 }
--- Inteiro/Banco/src/udp/Server.java
+++ Inteiro/Banco/src/udp/Server.java
@@ -7,79 +7,196 @@ import java.io.*;
 import java.net.*;
 import bancoDados.*;
 import construtor.*;
+import java.util.List;
 
 /**
  *
  * @author psiuzin
  */
-
 //1 Adiciona
 //2 Altera
 //3 Exclui
 //4 Consulta
 //5 ListaTipo
 //6 Mensagem Aleatória de Tipo
-
 public class Server {
-    public static void main(String args[]) throws Exception
-      {
-         DatagramSocket serverSocket = new DatagramSocket(2006);
-            byte[] receiveData = new byte[1024];
-            byte[] sendData = new byte[1024];
-            while(true)
-               {
-                  DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-                  serverSocket.receive(receivePacket);
-                  String sentence = new String(receivePacket.getData());
-                  System.out.println("Received: " + sentence);
-                  String[] split = sentence.split("&");
-                  char tipoop = split[0].charAt(0);
-                  String msg = split[1];
-                  int msgcod = Integer.parseInt(split[2]);
-                  int tipomsg = Integer.parseInt(split[3]);
-                  BelasMensagens bmsg = new BelasMensagens(msgcod, msg, tipomsg);
-                  
-                  switch(tipoop) {
-                      case '1': // Adiciona
-                          Operacoes.adicionaMsg(bmsg);
-                          System.out.println("Adiciona");
-                          break;
-                      case '2': // Altera
-                          Operacoes.alteraMsg(bmsg);
-                          System.out.println("Altera");
-                          break;
-                      case '3': // Exclui
-                          Operacoes.deletaMsg(bmsg);
-                          System.out.println("Exclui");
-                          break;
-                      case '4': // Consulta
-                          Operacoes.consultaMsg(bmsg.getCodigo());
-                          System.out.println("Consulta");
-                          break;
-                      case '5': // ListaTipo
-                          Operacoes.consultaTipo(tipomsg);
-                          System.out.println("Lista Tipo");
-                          break;
-                      case '6': // Msg Aleatória Tipo
-                          Operacoes.listamsg(tipomsg);
-                          System.out.println("Mensagem Aleatória de tal Tipo");
-                          break;
-                      default:
-                          System.out.println("Operação não válida");
-                  }
-                  
-                  
-                  
-                  
-                  
-                  
-                  InetAddress IPAddress = receivePacket.getAddress();
-                  int port = receivePacket.getPort();
-                  //String capitalizedSentence = sentence.toUpperCase();
-                  sendData = msg.getBytes();
-                  DatagramPacket sendPacket =
-                  new DatagramPacket(sendData, sendData.length, IPAddress, port);
-                  serverSocket.send(sendPacket);
-               }
-      }
+
+    private static String[] splitByNumber(String s, int size) { // Separa uma string em várias string de tamanho X
+        if (s == null || size <= 0) {
+            return null;
+        }
+        int chunks = s.length() / size + ((s.length() % size > 0) ? 1 : 0);
+        String[] arr = new String[chunks];
+        for (int i = 0, j = 0, l = s.length(); i < l; i += size, j++) {
+            arr[j] = s.substring(i, Math.min(l, i + size));
+        }
+        return arr;
+    }
+
+    public static void main(String args[]) throws Exception {
+        DatagramSocket serverSocket = new DatagramSocket(2006);
+        byte[] receiveData = new byte[1024];
+        byte[] sendData = new byte[1024];
+        String tipoop = new String();
+
+        String[] split;
+        while (true) {
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            serverSocket.receive(receivePacket);
+            String sentence = new String(receivePacket.getData()); // Recebi o tamanho da mensagem
+            String msg = new String();
+            BelasMensagens bmsg = new BelasMensagens();
+
+            split = sentence.split("#");
+            int qtdmsg = Integer.parseInt(split[0]);
+
+            if (qtdmsg == 1) // Se a mensagem for de tamanho 1 packet, faço tudo normalmente
+            {
+                serverSocket.receive(receivePacket);
+                sentence = new String(receivePacket.getData());
+                split = sentence.split("#");
+                if (split.length > 1) //Splitei, se houve split é o conjunto de dados
+                {
+                    tipoop = split[0];
+                    int msgcod = Integer.parseInt(split[1]);
+                    int tipomsg = Integer.parseInt(split[2]);
+                    bmsg.setCodigo(msgcod);
+                    bmsg.setTipo(tipomsg);
+                } else {
+                    msg += split[0]; // Se não houve split, é a BelaMSG
+                    bmsg.setMensagem(msg);
+                }
+            } else // Mensagem maior que 1 packet
+            {
+                int tam = Character.getNumericValue(sentence.charAt(0)); // Pego o tamanho da mensagem
+                for (int i = 0; i < tam; i++) // Fico recebendo a mensagem
+                {
+                    serverSocket.receive(receivePacket);
+                    sentence = new String(receivePacket.getData());
+                    split = sentence.split("#");
+                    if (split.length > 1) //Splitei, se houve split é o conjunto de dados
+                    {
+                        tipoop = split[0];
+                        int msgcod = Integer.parseInt(split[1]);
+                        int tipomsg = Integer.parseInt(split[2]);
+                        bmsg.setCodigo(msgcod);
+                        bmsg.setTipo(tipomsg);
+                    } else {
+                        msg += split[0]; // Se não houve split, é a BelaMSG;
+                    }
+                }
+                bmsg.setMensagem(msg); // Mensagem concatenada completa
+            }
+            // A partir desse momento o objeto BelaMensagem é pra estar pronto
+
+            //BelasMensagens bmsg = new BelasMensagens(msgcod, msg, tipomsg);
+//            while (sentence.charAt(0) == 8) {
+//                String pac = new String();
+//                pac += sentence.substring(1, sentence.length());
+//                receivePacket = new DatagramPacket(receiveData, receiveData.length);
+//
+//            }
+//            System.out.println("Received: " + sentence);
+//            String[] split = sentence.split("&");
+//            char tipoop = split[0].charAt(0);
+//            msg = split[1];
+//            int msgcod = Integer.parseInt(split[2]);
+//            int tipomsg = Integer.parseInt(split[3]);
+//            BelasMensagens bmsg = new BelasMensagens(msgcod, msg, tipomsg);
+            InetAddress IPAddress = receivePacket.getAddress();
+            int port = receivePacket.getPort();
+            String msgresposta = new String();
+            DatagramPacket sendPacket;
+            BelasMensagens bmsg2 = new BelasMensagens();
+
+            int operacao = Integer.parseInt(tipoop);
+            switch (operacao) {
+                case 404: // requisição se está vivo
+                    System.out.println("Tô vivo");
+                    String alive = new String("Alive!");
+                    sendData = alive.getBytes();
+                    sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                    serverSocket.send(sendPacket);
+                case 1: // Adiciona
+                    Operacoes.adicionaMsg(bmsg);
+                    System.out.println("Adiciona");
+                    break;
+                case 2: // Altera
+                    Operacoes.alteraMsg(bmsg);
+                    System.out.println("Altera");
+                    break;
+                case 3: // Exclui
+                    Operacoes.deletaMsg(bmsg);
+                    System.out.println("Exclui");
+                    break;
+                case 4: // Consulta
+                    bmsg2 = Operacoes.consultaMsg(bmsg.getCodigo());
+                    split = splitByNumber(bmsg2.getMensagem(), 110);
+                    System.out.println("Consulta");
+                    msgresposta = Integer.toString(split.length) + "#";
+                    sendData = msgresposta.getBytes();
+                    sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                    serverSocket.send(sendPacket); // Enviei quantos packets serão necessários para receber a mensagem
+
+                    for (int i = 0; i < split.length; i++) {
+                        msgresposta = split[i];
+                        sendData = msgresposta.getBytes();
+                        sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                        serverSocket.send(sendPacket);
+                    } // Envio todos os pacotes da mensagem
+                    break;
+                case 5: // ListaTipo
+                    List<BelasMensagens> lista;
+                    lista = Operacoes.listamsg(bmsg.getTipo());
+                    System.out.println("Lista Tipo");
+                    msgresposta = Integer.toString(lista.size()) + "#";
+                    sendData = msgresposta.getBytes();
+                    sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                    serverSocket.send(sendPacket); // Enviei quantas mensagens tem na lista
+                    for (int i = 0; i < lista.size(); i++) {
+                        bmsg2 = lista.get(i);
+                        split = splitByNumber(bmsg2.getMensagem(), 110);
+                        msgresposta = Integer.toString(split.length) + "#";
+                        sendData = msgresposta.getBytes();
+                        sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                        serverSocket.send(sendPacket); // Enviei quantos packets serão necessários para receber a mensagem
+
+                        for (int j = 0; j < split.length; j++) {
+                            msgresposta = split[i];
+                            sendData = msgresposta.getBytes();
+                            sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                            serverSocket.send(sendPacket);
+                        } // Envio todos os pacotes da mensagem
+                    }
+                    break;
+                case 6: // Msg Aleatória Tipo
+                    bmsg2 = Operacoes.consultaTipo(bmsg.getTipo());
+                    split = splitByNumber(bmsg2.getMensagem(), 110);
+                    System.out.println("Consulta");
+                    msgresposta = Integer.toString(split.length) + "#";
+                    sendData = msgresposta.getBytes();
+                    sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                    serverSocket.send(sendPacket); // Enviei quantos packets serão necessários para receber a mensagem
+
+                    for (int i = 0; i < split.length; i++) {
+                        msgresposta = split[i];
+                        sendData = msgresposta.getBytes();
+                        sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                        serverSocket.send(sendPacket);
+                    } // Envio todos os pacotes da mensagem
+                    System.out.println("Mensagem Aleatória de tal Tipo");
+                    break;
+                default:
+                    System.out.println("Operação não válida");
+            }
+
+//            InetAddress IPAddress = receivePacket.getAddress();
+//            int port = receivePacket.getPort();
+//            String capitalizedSentence = sentence.toUpperCase();
+//            sendData = msg.getBytes();
+//            DatagramPacket sendPacket
+//                    = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+//            serverSocket.send(sendPacket);
+        }
+    }
 }
