ec1c90409771fdb540735954b0d166879cc455a6
jaderhart <jaderhartt@gmail.com>
2016-11-13 03:25:47 -0200

    Add files via upload
    Atualizados com

--- Inteiro/WebServiceSoap/src/java/url/tela/cod/ServicoCliente.java
+++ Inteiro/WebServiceSoap/src/java/url/tela/cod/ServicoCliente.java
@@ -6,10 +6,15 @@
 package url.tela.cod;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.ejb.Stateless;
+import url.banco.BelasMensagens;
+import url.banco.Conexao;
+import url.banco.Operacoes;
 
 /**
  *
@@ -18,4 +23,75 @@ import javax.ejb.Stateless;
 @WebService(serviceName = "ServicoCliente")
 @Stateless()
 public class ServicoCliente {
+
+    /**
+     * Operação de Web service
+     */
+    @WebMethod(operationName = "adicionarMensagem")
+    public BelasMensagens adicionarMensagem(@WebParam(name = "codigo") int codigo, @WebParam(name = "mensagem") String mensagem, @WebParam(name = "tipo") int tipo) throws IOException {
+        //TODO write your implementation code here:
+        Operacoes op= new Operacoes();
+        BelasMensagens bm=new BelasMensagens();
+        bm.setCodigo(codigo);
+        bm.setMensagem(mensagem);
+        bm.setTipo(tipo);
+        //op.adicionaMsg(bm);
+        return bm;
+    }
+
+    /**
+     * Operação de Web service
+     */
+    @WebMethod(operationName = "alteraMensagem")
+    public BelasMensagens alteraMensagem(@WebParam(name = "codigo") int codigo, @WebParam(name = "mensagem") String mensagem, @WebParam(name = "tipo") int tipo) throws IOException {
+        //TODO write your implementation code here:
+        Operacoes op= new Operacoes();
+        BelasMensagens bm=new BelasMensagens();
+        bm.setCodigo(codigo);
+        bm.setMensagem(mensagem);
+        bm.setTipo(tipo);
+        //op.alteraMsg(bm);
+        return bm;
+    }
+
+    /**
+     * Operação de Web service
+     */
+    @WebMethod(operationName = "excluiMensagem")
+    public BelasMensagens excluiMensagem(@WebParam(name = "codigo") int codigo) throws IOException {
+        //TODO write your implementation code here:
+        return null;
+    }
+
+    /**
+     * Operação de Web service
+     */
+    @WebMethod(operationName = "consultaMensagem")
+    public BelasMensagens consultaMensagem(@WebParam(name = "codigo") int codigo) throws IOException {
+        //TODO write your implementation code here:
+        return null;
+    }
+
+    /**
+     * Operação de Web service
+     */
+    @WebMethod(operationName = "listaTipo")
+    public List listaTipo(@WebParam(name = "codigo") int codigo) throws IOException {
+        //TODO write your implementation code here:
+        return null;
+    }
+
+    /**
+     * Operação de Web service
+     */
+    @WebMethod(operationName = "aleatoriaMensagem")
+    public BelasMensagens aleatoriaMensagem(@WebParam(name = "tipo") int tipo) throws IOException {
+        //TODO write your implementation code here:
+        return null;
+    }
+
+
+    
+    
+    
 }
