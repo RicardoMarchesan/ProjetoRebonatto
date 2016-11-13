83331bad6529a2ef0c047764baa7b48b1082d8cb
jaderhart <jaderhartt@gmail.com>
2016-11-13 00:58:21 -0200

    Add files via upload

---
+++ Inteiro/WebServiceSoap/src/java/url/banco/Conexao.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package url.banco;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+
+
+public class Conexao {
+
+    public Statement stm;//responsavel por preparar e realizar pesquisa no banco
+    public ResultSet rs;//responsavel por armazenar o resulatdo de uma pesquisa
+    //private String drive = "org.postgresql.Driver"; //identifica o banco de dados que esta usando
+    static String caminho = "jdbc:postgresql://localhost:5432/BelasMensagens";//porta do postgres
+    static String usuario = "postgres";
+    static String senha = "mdl";
+    static Connection conn;
+
+    public static Connection conecta() throws SQLException {
+        try {//carrega o drive postgre
+            Class.forName("org.postgresql.Driver");
+            
+            conn = DriverManager.getConnection(caminho, usuario, senha);
+            //System.out.println("oiii");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver não encontrado!" + e);
+        }
+        return conn;
+    }
+
+    public static PreparedStatement getPrepared(String sql) throws SQLException {
+        try {
+            return conecta().prepareStatement(sql);
+        } catch (Exception e1) {
+            System.out.println("Erro ao preparar a instrução SQL!");
+            return null;
+        }
+
+    }
+    public static Statement statement (){
+        try {
+          return conecta().createStatement();
+        } catch (Exception e2) {
+           System.out.println("Erro ao criar statement");  
+           return null;
+        }
+   
+    }
+}
---
+++ Inteiro/WebServiceSoap/src/java/url/tela/cod/ServicoCliente.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package url.tela.cod;
+
+import java.io.IOException;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Jader
+ */
+@WebService(serviceName = "ServicoCliente")
+@Stateless()
+public class ServicoCliente {
+}
